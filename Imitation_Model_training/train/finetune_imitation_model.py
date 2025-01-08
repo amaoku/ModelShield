851ea99f24aa5bcec4642e40375805abff6f78a6
amaoku <pky22@mails.tsinghua.edu.cn>
2025-01-08 09:47:07 +0800

    Update and rename finetune_llama2_Wild.py to finetune_imitation_model.py

--- Imitation_Model_training/train/finetune_imitation_model.py
+++ Imitation_Model_training/train/finetune_imitation_model.py
@@ -232,7 +232,7 @@ def train(
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_config_file", type=str,default='/data1/pky/BELLE/train/configs/baseline/llama2_baseline_wild.json')
+    parser.add_argument("--model_config_file", type=str,default='/your/path/to/fine-tuning/imitation/model/config.json')
     parser.add_argument("--deepspeed", type=str, help="deepspeed config")
     parser.add_argument("--resume_from_checkpoint", action="store_true", default=False)
     parser.add_argument("--lora_hyperparams_file", default="", type=str, help="Provide it when use_lora=True")
