07784367629532bb6cf821a35e04f01c9c4c6018
amaoku <pky22@mails.tsinghua.edu.cn>
2025-01-08 09:36:31 +0800

    Delete Imitation_Model_training/train/run_finetune.sh

--- Imitation_Model_training/train/run_finetune.sh
+++
@@ -1,3 +0,0 @@
-
-python /data1/pky/BELLE/train/finetune_mistral_WM.py  --model_config_file='/data1/pky/BELLE/train/configs/mistral/mistral_WM_hc3.json'
-python /data1/pky/BELLE/train/finetune_mistral_WM.py --model_config_file='/data1/pky/BELLE/train/configs/mistral/mistral_WM_wild.json'
